8b444215201f75a8aa16826f5bcde7e7e10b9c74
Varga Mihály <mihaly2001@gmail.com>
2022-09-29 19:51:27 +0200

    commir changes

---
+++ UserMaintenance/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        
+        public string FullName { get; set; }
+
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName, FirstName);
+            }
+        }
+    }
+}
+
+
