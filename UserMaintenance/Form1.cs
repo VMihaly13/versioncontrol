2a26a627f8405b80f3247dfa1b7f25b4304b6832
Varga Mihály <mihaly2001@gmail.com>
2022-09-29 20:03:28 +0200

    commit

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,17 @@ namespace UserMaintenance
 
 
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sr = new StreamWriter(users, Encoding.Default))
+            {
+                users.Write(ID);
+                users.Write(FullName);
+                sr.WriteLine();
+
+            }
         }
     }
 }
