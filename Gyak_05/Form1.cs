0835a4c64d50b1bb45126e7e749d941a87011121
Varga Mihály <mihaly2001@gmail.com>
2022-10-20 22:01:19 +0200

    asd

--- Gyak_05/Form1.cs
+++ Gyak_05/Form1.cs
@@ -16,6 +16,8 @@ namespace Gyak_05
         {
             InitializeComponent();
             Calling();
+            Use();
+            Show();
         }
 
         private void Calling()
@@ -30,10 +32,56 @@ namespace Gyak_05
                 };
 
 
-    var response = mnbService.GetExchangeRates(request);
+            var response = mnbService.GetExchangeRates(request);
 
 
-    var result = response.GetExchangeRatesResult;
+                var result = response.GetExchangeRatesResult;
         }
+
+
+        private void Use()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+
+            foreach (XmlElement element in xml.DocumentElement)
+                {
+            var rate = new RateData();
+            Rates.Add(rate);
+
+
+            rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+
+            var childElement = (XmlElement)element.ChildNodes[0];
+            rate.Currency = childElement.GetAttribute("curr");
+
+
+            var unit = decimal.Parse(childElement.GetAttribute("unit"));
+            var value = decimal.Parse(childElement.InnerText);
+            if (unit != 0)
+            rate.Value = value / unit;
+        }
+
+
+            private void Show()
+            {
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];            
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
+            }
     }
 }
