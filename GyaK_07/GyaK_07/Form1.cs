920cddcacb6cfea94bf8835fdadb2c99e68b4e07
Mihály Varga <mihaly.varga@stud.uni-corvinus.hu>
2022-11-11 10:01:07 +0100

    commit all second

---
+++ GyaK_07/GyaK_07/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GyaK_07
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'portfolioDataSet.Tick' table. You can move, or remove it, as needed.
+            this.tickTableAdapter.Fill(this.portfolioDataSet.Tick);
+
+        }
+    }
+}
